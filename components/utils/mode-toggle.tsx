2484c890f744e4059f1b05d1c238ef090606cd03
programfive <kennedysuedavid24@gmail.com>
2025-01-26 13:51:36 -0400

    MoonIcon erro

--- components/utils/mode-toggle.tsx
+++ components/utils/mode-toggle.tsx
@@ -1,9 +1,8 @@
 "use client"
 
 import * as React from "react"
-import { Moon, MoonIcon, Sun } from "lucide-react"
+import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
-
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
